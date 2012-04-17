3c6afe1202a0ab2e4a9ec6cfd520db54acdc3396
Paul <thepaulxxx@gmail.com>
2012-04-17 23:32:41 +0600

    robyn: a lot changes, look mimmi

--- cm.mk
+++ cm.mk
@@ -1,16 +1,17 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := E10i
+# Inherit some common OXP stuff.
+$(call inherit-product, vendor/oxp/common_oxp_phone.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/semc/robyn/device_robyn.mk)
 
 # Setup device configuration
+PRODUCT_RELEASE_NAME := E10i
 PRODUCT_DEVICE := robyn
 PRODUCT_NAME := cm_robyn
 PRODUCT_BRAND := SEMC
@@ -23,4 +24,4 @@ TARGET_NO_LIVEWALLPAPERS := false
 TARGET_BOOTANIMATION_NAME := vertical-240x320
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=E10i BUILD_ID=ITL41F BUILD_DISPLAY_ID=ITL41F BUILD_FINGERPRINT="SEMC/ST18i_0000-0000/ST18i:4.0.3/4.1.C.0.7/-H9_3w:user/release-keys" PRIVATE_BUILD_DESC="ST18i-user 4.0.3 4.1.C.0.7 -H9_3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=E10i BUILD_ID=4.1.A.0.562 BUILD_DISPLAY_ID=ITL41F BUILD_FINGERPRINT="SEMC/LT18i_1255-1753/LT18i:4.0.3/4.1.A.0.572/zf1_3w:user/release-keys" PRIVATE_BUILD_DESC="LT18i-user 4.0.3 4.1.A.0.562 zf1_3w test-keys"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,3 +19,4 @@
 # function: add_lunch_combo generic-eng
 
 add_lunch_combo cm_robyn-eng
+add_lunch_combo cm_robyn-userdebug
